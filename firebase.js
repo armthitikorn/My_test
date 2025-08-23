2d7fffbc2325834420ed36c4c11cd6b6635dc126
armthitikorn <armthitikorn@gmail.com>
2025-08-23 21:25:59 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyA4OrZERUoxWPiShCEWVvz1MK9cYGO3_K8",
     authDomain: "new-record-3ccd6.firebaseapp.com",
     projectId: "new-record-3ccd6",
-    storageBucket: "new-record-3ccd6.appspot.com", // แก้ไขตรงนี้
+    storageBucket: "new-record-3ccd6.appspot.com", // แก้ไขกลับมาเป็นค่าที่ถูกต้อง
     messagingSenderId: "1065863160189",
     appId: "1:1065863160189:web:25313e55b0733f5e126c14",
     measurementId: "G-JW6N2HYR58"
@@ -21,4 +21,5 @@ const app = initializeApp(firebaseConfig);
 
 // เริ่มต้นบริการต่างๆ และ export เพื่อนำไปใช้ในไฟล์อื่น
 export const storage = getStorage(app);
+
 export const db = getFirestore(app);
